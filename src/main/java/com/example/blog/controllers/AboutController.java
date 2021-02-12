7961fb2830bd4516fe1e5f650e84d1ef223f458e
Anton Bocharov <a.bocharov00@mail.ru>
2021-02-12 14:58:07 +0300

    mysqlconnection

---
+++ src/main/java/com/example/blog/controllers/AboutController.java
@@ -0,0 +1,2 @@
+package com.example.blog.controllers;public class AboutController {
+}
--- src/main/java/com/example/blog/controllers/BlogController.java
+++ src/main/java/com/example/blog/controllers/BlogController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BlogController {
     @GetMapping("/blog")
     public String blog(Model model){
+        model.addAttribute("title", "Блог");
         return "blog";
     }
 }
