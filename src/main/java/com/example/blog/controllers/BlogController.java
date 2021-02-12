93b4a213ba4feeb6eed4ba9b21f7063ca451cacc
Anton Bocharov <a.bocharov00@mail.ru>
2021-02-12 15:36:46 +0300

    modelFirst

--- src/main/java/com/example/blog/controllers/BlogController.java
+++ src/main/java/com/example/blog/controllers/BlogController.java
@@ -1,14 +1,22 @@
 package com.example.blog.controllers;
 
+import com.example.blog.models.Post;
+import com.example.blog.repos.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BlogController {
+
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/blog")
     public String blog(Model model){
-        model.addAttribute("title", "Блог");
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "blog";
     }
 }
---
+++ src/main/java/com/example/blog/repos/PostRepository.java
@@ -0,0 +1,8 @@
+package com.example.blog.repos;
+
+import com.example.blog.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long>{
+
+}
