dbaf0dc2cd119f9b8d812d08f6caa73c3ca38d3a
Anton Bocharov <a.bocharov00@mail.ru>
2021-02-18 00:17:50 +0300

    blogDetails

--- src/main/java/com/example/blog/controllers/BlogController.java
+++ src/main/java/com/example/blog/controllers/BlogController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -26,6 +30,17 @@ public class BlogController {
     public String blogAdd(Model model){
         return "blogAdd";
     }
+
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model){
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res= new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blogDetails";
+    }
+
     @PostMapping("/blog/add")
     public String blogPostAdd(@RequestParam String title, @RequestParam String announcement, @RequestParam String full_text, Model model){
         Post post = new Post(title,  announcement, full_text);
